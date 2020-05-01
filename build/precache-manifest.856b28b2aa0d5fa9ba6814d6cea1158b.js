884fdcc924bc30b433467222552c957f59d7c842
Ayush Jain <jn.ayush@gmail.com>
2020-05-01 01:20:22 -0400

    Removed cname

---
+++ build/precache-manifest.856b28b2aa0d5fa9ba6814d6cea1158b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d825580fe74832a3af444e9ba43bc96",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "df1d86e7dd2bb4a970ea",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1cccddbc6c1996d48c43",
+    "url": "/portfolio/static/js/2.9a00532c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9a00532c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df1d86e7dd2bb4a970ea",
+    "url": "/portfolio/static/js/main.a744a8ed.chunk.js"
+  },
+  {
+    "revision": "9e7b935c892478eb64c5",
+    "url": "/portfolio/static/js/runtime-main.ddb13fc3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
